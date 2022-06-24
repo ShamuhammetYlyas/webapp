343f0c1b5b9258c9d5ed32b348d7a7d4348e0e76
ShamuhammetYlyas <shamuhammetylyas@ynamly.com.tm>
2022-06-24 12:30:39 +0500

    Created our first web server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", welcome)
+
+	http.ListenAndServe(":3000", nil)
 }
