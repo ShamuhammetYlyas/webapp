450f5ff2fb022b2e0c25a9b75102c2737ee61c64
ShamuhammetYlyas <shamuhammetylyas@ynamly.com.tm>
2022-06-25 09:43:26 +0500

    Added a content-type header

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", welcome)
-
+	fmt.Println("Server starting on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
