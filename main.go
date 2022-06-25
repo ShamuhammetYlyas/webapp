08bc6382f1170a171adb61ab548331892f9867ae
ShamuhammetYlyas <shamuhammetylyas@ynamly.com.tm>
2022-06-25 13:34:50 +0500

    Installed gorilla/mux package for routing

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func welcome(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<h1>Contact page!</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>Page not found</h1>")
-	}
+	fmt.Fprint(w, "<h1>Home page</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact page</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", welcome)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
 	fmt.Println("Server starting on port 3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
